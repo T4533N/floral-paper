0a00a36c0e06104dfb5a7e7cd164be18fd3e5c86
Taseen Tanvir <44163644+T4533N@users.noreply.github.com>
2024-06-15 19:33:44 +0000

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24 gap-5">
+      <div className="text-3xl font-semibold">Coming Soon...</div>
+      <img src={"/logo.webp"} className="w-[100px]" alt="Logo" />
+    </main>
+  );
+}
